71bf6918692d50aca9ce5c0cee2b5cac1b649f25
antanandia33 <antanandia@gmail.com>
2023-03-12 21:07:56 -0700

    added version 1

---
+++ EtchASketch.js
@@ -0,0 +1,53 @@
+function clearGrid(size) {
+  while(grid.firstChild) {
+    grid.removeChild(grid.firstChild);
+  }
+  setupGrid(size);
+}
+
+function changeColor(e) {
+  if(e.type === 'mouseover' && !mouseDown) {
+    return;
+  }
+  e.target.style.backgroundColor = '#2B303A';
+}
+
+function setupGrid(gridSize) {
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+  for (let i = 0; i < gridSize*gridSize; i++) {
+    const gridElement = document.createElement('div');
+    gridElement.classList.add('gridElement');
+    gridElement.addEventListener('mouseover', changeColor);
+    gridElement.addEventListener('mousedown', changeColor);
+    grid.appendChild(gridElement);
+  }
+}
+
+const grid = document.getElementById('grid');
+const clearButton = document.getElementById('clearButton');
+const smallButton = document.getElementById('smallButton');
+const mediumButton = document.getElementById('mediumButton');
+const largeButton = document.getElementById('largeButton');
+let size = 16;
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+clearButton.onclick= () => clearGrid(size);
+smallButton.onclick= () => {
+  size = 64;
+  clearGrid(size); 
+}
+mediumButton.onclick= () => {
+  size = 32;
+  clearGrid(size) 
+}
+largeButton.onclick= () => {
+  size = 16;
+  clearGrid(size) 
+}
+
+window.onload = () => {
+  setupGrid(size);
+}
+
