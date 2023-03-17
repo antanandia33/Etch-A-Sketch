c601a95084cfd199b739834060827a863b1def47
antanandia33 <antanandia@gmail.com>
2023-03-17 12:09:11 -0700

    added an active status to the size buttons and added an eraser feature

--- EtchASketch.js
+++ EtchASketch.js
@@ -9,8 +9,12 @@ function changeColor(e) {
   if(e.type === 'mouseover' && !mouseDown) {
     return;
   }
-  color = document.getElementById('colorChooser').value;
-  e.target.style.backgroundColor = color;
+  if (eraserButton.classList.contains('active')) {
+    e.target.style.backgroundColor = '#ffffff';
+  } else if (!eraserButton.classList.contains('active')) {
+    color = document.getElementById('colorChooser').value;
+    e.target.style.backgroundColor = color;
+  }
 }
 
 function setupGrid(gridSize) {
@@ -25,7 +29,26 @@ function setupGrid(gridSize) {
   }
 }
 
+function activeSizeButton(gridSize) {
+  if (smallButton.classList.contains('active')) {
+    smallButton.classList.remove('active');
+  } else if (mediumButton.classList.contains('active')) {
+    mediumButton.classList.remove('active');
+  } else if (largeButton.classList.contains('active')) {
+    largeButton.classList.remove('active');
+  }
+
+  if (gridSize == 'small') {
+    smallButton.classList.add('active');
+  } else if (gridSize == 'medium') {
+    mediumButton.classList.add('active');
+  } else if (gridSize == 'large') {
+    largeButton.classList.add('active');
+  }
+}
+
 const grid = document.getElementById('grid');
+const eraserButton = document.getElementById('eraserButton');
 const clearButton = document.getElementById('clearButton');
 const smallButton = document.getElementById('smallButton');
 const mediumButton = document.getElementById('mediumButton');
@@ -35,18 +58,23 @@ let size = 16;
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
+
+eraserButton.onclick= () => eraserButton.classList.toggle('active');
 clearButton.onclick= () => clearGrid(size);
 smallButton.onclick= () => {
   size = 64;
-  clearGrid(size); 
+  clearGrid(size);
+  activeSizeButton('small');
 }
 mediumButton.onclick= () => {
   size = 32;
-  clearGrid(size) 
+  clearGrid(size);
+  activeSizeButton('medium'); 
 }
 largeButton.onclick= () => {
   size = 16;
-  clearGrid(size) 
+  clearGrid(size);
+  activeSizeButton('large'); 
 }
 
 window.onload = () => {
